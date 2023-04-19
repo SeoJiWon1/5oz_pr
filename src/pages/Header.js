85ceca28d1fedde0e7fe8e0aeb82dacb19c0ea7e
SeoJiWon1 <dhomp7730@gmail.com>
2023-04-19 16:24:42 +0900

    Initialize project using Create React App

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,27 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import { BsFillBellFill } from "react-icons/bs";
 import { BsFillGearFill } from "react-icons/bs";
-
+import { FaHatWizard } from "react-icons/fa";
 
 function Header() {
   return (
-    <Nav className =" Nav-all"
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item className = "Nav-logo">
-        <Nav.Link href="/home" className = "Logo">5Oz Software</Nav.Link>
+    <Nav className =" Nav-all">
+      <Nav.Item>
+        <Nav.Link href="/" className = "Logo"><FaHatWizard></FaHatWizard> 5Oz Software</Nav.Link>
       </Nav.Item>
-
       <Nav className='Nav-type'>
         <Nav.Item>
-          <Nav.Link eventKey="link-1">App Name</Nav.Link>
+          <Nav.Link eventKey="link-1">내 작업</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-2">My project</Nav.Link>
+          <Nav.Link eventKey="link-2">프로젝트</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-3">프로젝트</Nav.Link>
+          <Nav.Link eventKey="link-3">팀</Nav.Link>
         </Nav.Item>
         
 
