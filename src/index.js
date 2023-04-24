d708ecfdecfd613a259a7dcde4dfc9e72627a9f4
SeoJiWon1 <dhomp7730@gmail.com>
2023-04-24 18:06:54 +0900

    status 에러문 추가

--- src/index.js
+++ src/index.js
@@ -7,8 +7,34 @@ import {BrowserRouter} from 'react-router-dom';
 import { Provider } from "react-redux";
 import  { store, persistor } from './redux/Store'
 import {PersistGate} from "redux-persist/integration/react";
-import persistStore from "redux-persist/es/persistStore"
+import axios from 'axios';
 
+axios.interceptors.response.use(
+  function (response) {
+  /*
+      http status가 200인 경우
+      응답 성공 직전 호출됩니다. 
+      .then() 으로 이어집니다.
+  */
+    return response;
+  },
+  function (error) {
+  /*
+      http status가 200이 아닌 경우
+      응답 에러 직전 호출됩니다.
+      .catch() 으로 이어집니다.    
+  */
+    //401은 Access Token or Refresh Token 이 invalid 될때
+    //response data의 code값이 
+    // 4401 : access Token error , 4402: refresh Token error
+    if(error.response.status === 401){
+      if(error.response.data.code === '4401'){
+        window.location.href= '/'; 
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
