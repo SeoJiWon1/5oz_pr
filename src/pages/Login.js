be965f6b4eb3fdefe7127f0e86448851917f145c
SeoJiWon1 <dhomp7730@gmail.com>
2023-04-24 18:08:47 +0900

    accesstoken, refreshtoken setTimeout 문 추가

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,16 +4,15 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState } from "react";
 import axios from'axios';
-import { useDispatch } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor } from '../redux/Store';
-import { setToken } from '../path/to/setToken'; // setToken 함수를 import
+// import { setToken } from '../path/to/setToken'; // setToken 함수를 import
 import { useNavigate } from "react-router-dom";
+import refreshToken from "../path/to/utils";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   
@@ -27,21 +26,25 @@ function Login() {
 
   function handleSubmit(e) {
     e.preventDefault();
+    const data = {username, password};
     // if (!username || !password ){
     //   alert('이메일과 비밀번호를 적어주세요');
     // }
-    axios.post('http://localhost:8080/api/post/01', { username, password }, {
+    axios.post('http://localhost:8080/api/post/01',data, {
     headers: {
     'Content-Type': 'application/json'
     }
     })
-    // 로그인 요청을 보내고 JWT 토큰을 받아옴
     .then(response => {
-      localStorage.setItem('token', response.data.token);
-      // JWT 토큰을 로컬 스토리지에 저장
-      dispatch(setToken(response.data.token)); // redux 액션 호출하여 토큰 상태 업데이트
+      axios.defaults.headers.common['Authorization']=`Bearer ${response.data.accesstoken}`; //'Bearer' + response.data;
+      // 'axios.post요청이 성공했을 때 'Authorization' 헤더에  Bearer 타입의 acceesstoken 값을 설정하여 서버에 인증을 요청하고 있는 것
+      localStorage.setItem('accesstoken', response.data.accesstoken);
+      // access 토큰을 로컬 스토리지에 저장
       navigate("/ProjectSelect"); // "/ProjectSelect" 경로로 페이지 이동
       // 로그인 성공 후의 처리를 진행 (예 : 페이지 이동)
+      setTimeout(function(){
+        refreshToken(null);
+      }, (60 * 1000));
     })
     .catch(error => {
       alert('존재하지 않는 아이디이거나 비밀번호가 다릅니다.')
