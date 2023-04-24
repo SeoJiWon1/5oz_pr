9afcb5672e4f46b158fe3c1e2274baf4108c6d5a
SeoJiWon1 <dhomp7730@gmail.com>
2023-04-24 18:07:25 +0900

    Modal 추가

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,15 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import { BsFillBellFill } from "react-icons/bs";
 import { BsFillGearFill } from "react-icons/bs";
-import { FaHatWizard } from "react-icons/fa";
+import { FaHatWizard, FaUser} from "react-icons/fa";
+import React,{useState} from 'react';
 
 function Header() {
+
+  const [modal, setModal] = useState(false);
+  
+
   return (
-    <Nav className =" Nav-all">
+    <Nav className ="nav-all">
       <Nav.Item>
-        <Nav.Link href="/" className = "Logo"><FaHatWizard></FaHatWizard> 5Oz Software</Nav.Link>
+        <Nav.Link href="/" className = "logo"><FaHatWizard></FaHatWizard> 5Oz Software</Nav.Link>
       </Nav.Item>
-      <Nav className='Nav-type'>
+      <Nav className='nav-type'>
         <Nav.Item>
           <Nav.Link eventKey="link-1">내 작업</Nav.Link>
         </Nav.Item>
@@ -23,29 +28,47 @@ function Header() {
         </Nav.Item>
         
 
-        <Nav className ='Nav-setting'>
-          <Nav.Item className = "Nav-icon">
+        <Nav className ='nav-setting'>
+          <Nav.Item className = "nav-icon">
             <BsFillBellFill/>
           </Nav.Item>
           <Nav.Item >
             <Nav.Link eventKey="link-4"> 알림</Nav.Link>
           </Nav.Item>  
-          <Nav.Item className = "Nav-icon">
+          <Nav.Item className = "nav-icon">
             <BsFillGearFill/>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link eventKey="link-5">설정</Nav.Link>
           </Nav.Item>
+          <Nav.Item className = "nav-icon">
+            <FaUser/>
+          </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-6">계정</Nav.Link>
+            <Nav.Item >
+              <div onClick={()=> {modal == true ? setModal(false) : setModal(true)}}>
+                계정
+              </div>
+              <div>
+                {modal == true ? <Modal/> : null}
+              </div>
+            </Nav.Item>
           </Nav.Item>
         </Nav>
       </Nav>
     </Nav>
-    
-    
-    
   );
 }
+
+
+function Modal(){
+  return(
+    <div className='modal-logout'>
+      로그아웃
+    </div>
+
+  )
+}
+
 
 export default Header;
