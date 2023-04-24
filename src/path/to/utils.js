822a33ae62c849b3355fa7e44fae818feae1c293
SeoJiWon1 <dhomp7730@gmail.com>
2023-04-24 18:09:22 +0900

    refreshtoken 컴포넌트 추가

---
+++ src/path/to/utils.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const refreshToken = (callback) => {
+  axios.post("/auth/refreshToken", {
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+  .then(response => {
+    console.log("res.data.accesstoken : " + response.data);
+    axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.acceesstoken}`;
+    if (callback) {
+      callback(true);
+    }
+
+    setTimeout(() => {
+      refreshToken(null);
+      // 60초가 지난 후에 refreshToken 함수 작용 그 refreshToken함수는 토큰을 다시 받아옴
+      // 자동으로 accesstoken 갱신
+    }, (60 * 1000));
+  })
+  .catch(error => {
+    console.log("app silent requset fail : " + error);
+    if (callback) {
+      callback(false);
+    }
+  })
+  .finally(() => {
+    console.log("refresh token request end");
+    // setLoading(true);
+  });
+};
+
+export default refreshToken;
