348f71bec91a610c036640275b9049e54a3d8987
SeoJiWon1 <dhomp7730@gmail.com>
2023-04-27 00:00:59 +0900

    setupProxy.js추가

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+// src/setupProxy.js
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/me', 
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+    })
+  );
+};
