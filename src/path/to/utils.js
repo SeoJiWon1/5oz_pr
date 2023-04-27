4cf19aa02e5d728946bbc91e9ebe1fe5618d8209
SeoJiWon1 <dhomp7730@gmail.com>
2023-04-27 18:30:55 +0900

    path수정

--- src/path/to/utils.js
+++ src/path/to/utils.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const refreshToken = (callback) => {
-  axios.post("/auth/refreshToken", {
+  axios.post("http://localhost:8080/me/login", {
     headers: {
       "Content-Type": "application/json",
     }
