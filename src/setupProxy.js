3d9b7b8087673b454222326f0daf15d9e7721c2b
SeoJiWon1 <dhomp7730@gmail.com>
2023-04-27 18:30:17 +0900

    CORS 관련

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,9 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/me', 
+    "http://localhost:8080/me/login", 
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: '//http://localhost:8080',
       changeOrigin: true,
     })
   );
