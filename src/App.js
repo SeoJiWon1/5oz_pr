7d46c87df69c5edc6a0185c1ac19ef61ff119cbb
SeoJiWon1 <dhomp7730@gmail.com>
2023-05-04 14:35:36 +0900

    Router 수정

--- src/App.js
+++ src/App.js
@@ -3,52 +3,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './pages/Header';
 import Login from './pages/Login.js';
 import Slidebar from './pages/Slidebar.js';
-import Nav from 'react-bootstrap/Nav';
 import {Routes, Route, Link} from 'react-router-dom';
 import Sidenav from "./pages/Sidenav";
 import ProjectCreate from "./pages/ProjectCreate";
 import Board from "./pages/Board";
-
+import Firstpage from "./pages/Firstpage";
+import ProjectSelect from './pages/ProjectSelect';
 
 function App() {
 
   return (
       <Routes>
+        {/* 첫 시작 화면 */}
         <Route path = "/" element ={
           <div>
-            <Nav className =" header-all">
-              <Nav.Item className = "header-logo">
-                <Nav.Link href="/">5OzSoftware</Nav.Link>
-                <Nav.Item >
-                  <Nav.Link href="/Login"><button className="header-login">로그인</button></Nav.Link>
-                  <Link to="/SignUp"><button className="header-register">회원가입</button></Link>
-                </Nav.Item>   
-              </Nav.Item>  
-            </Nav>
-        
-        <div className = "main-text">5Oz Sofware의 제품을 만나보세요! </div>
+        <Firstpage></Firstpage>
         <Slidebar></Slidebar>          
           </div>
         }/>
+
+        {/* 로그인 페이지 */}
         <Route path ="/Login" element={<Login />} />
+
+        {/* 로그인 했을 때 프로젝트 선택창 */}
         <Route path = "ProjectSelect" element = {
           <div>
             <Header/>
-            <div className='header-container'>
-              <div className = "header-text">
-                프로젝트
-              </div>
-            <Link to ="/ProjectCreate"><button className="header-btncrt">프로젝트 만들기</button></Link>
-            </div>
+            <ProjectSelect/>
           </div>}
         />
+
+        {/* 프로젝트 새로 만들기 */}
         <Route path = "/ProjectCreate" element={<ProjectCreate/>}/>
+
+        {/* 프로젝트 선택해서 들어갔을 때 */}
         <Route path ="/Main" element={[
           <Header></Header>, 
           <Sidenav></Sidenav>,
           <Board></Board>
-          
-          
         ]} />
         
       </Routes>
