4db513f4b0cfe9a69a00d2ac8aa7d1ab55ec0850
SeoJiWon1 <dhomp7730@gmail.com>
2023-05-07 01:37:44 +0900

    accesstoken요청

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,22 +6,22 @@ import Form from 'react-bootstrap/Form';
 import axios from'axios';
 import { useNavigate } from "react-router-dom";
 //import refreshToken from "../path/to/utils";
-import { setLoggedIn } from "../store/authSlice";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+//import { setLoggedIn } from "../store/authSlice";
+// import { useDispatch } from "react-redux";
+// import { useSelector } from "react-redux";
 
 
 function Login() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  //const dispatch = useDispatch();
+  //const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const originData = 'client_id:client_secret';
     
 
     //accesstoken 요청 함수
     function getToken(){
       //const expiresIn = localStorage.getItem(res.data.expires_in); // 토큰 만료 시간
-      
+
       console.log(window.btoa(originData));
   
       axios.post('http://localhost:8080/oauth/token?grant_type=password&username=test1&password=test2',
@@ -36,7 +36,6 @@ function Login() {
           localStorage.setItem("access_token", res.data.access_token);
           localStorage.setItem("refresh_token", res.data.refresh_token);
           navigate("/ProjectSelect");
-          getMe();
         // handleLoginSuccess(res);
         
         }
@@ -56,80 +55,6 @@ function Login() {
     
   // }
  
-  function getMe(res){
-
-    const accessToken = localStorage.getItem("access_token");
-    axios.get('http://localhost:8080/me',
-    {
-      headers:{
-        Authorization: `Bearer ${accessToken}`
-      },
-    })
-    .then(res => {
-      console.log(res.status);
-      if(res.status !== 200){
-          getRefresh();
-      }else{
-        console.log('로그인 성공');
-      }
-      
-    })
-    .catch((error)=> {
-      console.log(error);
-      // if (error.response.status === 401) {
-      //   axios
-      //       .post(
-      //         "http://localhost:8080/oauth/token?grant_type=password&username=test1&password=test2",
-      //         {},
-      //         {
-      //           headers: {
-      //             'Authorization': 'Basic '+window.btoa(originData)
-      //           },
-      //         }
-      //       )
-
-      // }
-    })
-  }
-
-  function getRefresh(){
-
-    const refreshToken = localStorage.getItem("refresh_token");
-    axios.post('http://localhost:8080/oauth/token?grant_type=refresh_token&refresh_token='+refreshToken,
-    {
-      headers:{
-        'Authorization': 'Basic '+window.btoa(originData)
-      },
-    })
-    .then(res => {
-      console.log(res.status);
-      if(res.status === 200){
-          localStorage.setItem("access_token", res.data.access_token);
-          localStorage.setItem("refresh_token", res.data.refresh_token);
-      }else{
-        console.log('logout');
-      }
-      
-    })
-    .catch((error)=> {
-      console.log(error);
-      // if (error.response.status === 401) {
-      //   axios
-      //       .post(
-      //         "http://localhost:8080/oauth/token?grant_type=password&username=test1&password=test2",
-      //         {},
-      //         {
-      //           headers: {
-      //             'Authorization': 'Basic '+window.btoa(originData)
-      //           },
-      //         }
-      //       )
-
-      // }
-    })
-  }
-
-
   return (    
       <div className="Login-all">
         <div className="Login-Logo">5oz</div>
