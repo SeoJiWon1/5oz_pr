ef4868ce39d47a4da1b84339872fc42cc7486c5d
Seo_Ji Won <120435947+SeoJiWon1@users.noreply.github.com>
2023-05-09 18:01:34 +0900

    게시판 추가

--- src/pages/ProjectSelect.js
+++ src/pages/ProjectSelect.js
@@ -1,71 +1,55 @@
 import './ProjectSelect.css';
 import axios from'axios';
-import { useNavigate } from "react-router-dom";
+import { Await, useNavigate } from "react-router-dom";
+import {useEffect, useState} from 'react';
 
 function ProjectSelect(){
     const navigate = useNavigate();
-    const originData = 'client_id:client_secret';
+    const [projects, setProjects] = useState(null);
 
-    function getMe(){
-        const accessToken = localStorage.getItem("access_token");
-        console.log(accessToken);
-        axios.get('http://localhost:8080/me',
-        {
-          headers:{
-            Authorization: `Bearer ${accessToken}`
-          },
-        })
-        .then(res => {
-          console.log(res.status);
-          if(res.status === 200){
-              console.log('access_token이 유효합니다.');
-              navigate("/ProjectCreate");
-          }
-          
-        })
-        .catch((error)=> {
-          console.log(error);
-          if(error.response.status ===401 ){
-            console.log('401 에러입니다');
-            getRefresh();
-          }
-        })
-    
-        function getRefresh(){
-            const refreshToken = localStorage.getItem("refresh_token");
-            console.log(refreshToken);
-            axios.post('http://localhost:8080/oauth/token?grant_type=refresh_token&refresh_token='+refreshToken,
-            {},
-            {
-                headers:{
-                'Authorization': 'Basic '+window.btoa(originData)}
-            }
-            ).then(res => {
-                console.log(res);
-                if(res.status === 200){
-                    localStorage.setItem("access_token", res.data.access_token);
-                    localStorage.setItem("refresh_token", res.data.refresh_token);
-                    navigate("/ProjectCreate");
-                }else{
-                console.log('logout');
-                navigate("/Login");
-                }
-                
-            })
-            .catch((error)=> {
-                console.log(error);
-            })
-            }
-         }
+    // 만들기 눌렀을 때 이동함수    
+    function navigateProjectCreate(){
+        navigate('/ProjectCreate');
+      }
+         
+      async function getProject() {
+      try {
+        const response = await axios.get('http://localhost:8080/projects');
+        setProjects(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
     return(
+        <div>
+          <div className='header-container'>
+              <div className = "header-text">
+                  프로젝트
+              </div>
+             
+              <button className="header-btncrt" onClick ={navigateProjectCreate}>프로젝트 만들기</button>
+          </div>
 
-        <div className='header-container'>
-            <div className = "header-text">
-                프로젝트
-            </div>
-            <button className="header-btncrt" onClick ={getMe}>프로젝트 만들기</button>
+          <div>
+            여기도 공간입니다
+              <ul onClick={getProject}>
+              <hr></hr>
+                이곳
+              {projects && projects.map((project) => (
+              <li key={project.seq}>{project.seq}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{project.title}<hr></hr></li>
+            ))}
+              </ul>
+          </div>
         </div>
+
+
+
+
+            
+          
+
+            
     );
 
 }
