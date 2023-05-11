5e0a21a402a51f3913bcbadedd5bf9fd73888bcf
Seo_Ji Won <120435947+SeoJiWon1@users.noreply.github.com>
2023-05-11 15:24:12 +0900

    getMe함수 useEffect추가, deadline추가

--- src/pages/BacklogSelect.js
+++ src/pages/BacklogSelect.js
@@ -1,70 +1,82 @@
 import './BacklogSelect.css';
 import axios from'axios';
-import { useNavigate } from "react-router-dom";
+import GetMe from './GetMe';
+import {Link, useNavigate } from 'react-router-dom';
+import {useEffect, useState} from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function BacklogSelect(){
-    const navigate = useNavigate();
-    const originData = 'client_id:client_secret';
+    useEffect(() => {
+      GetMe();
+  }, [] );
 
-    function getMe(){
-        const accessToken = localStorage.getItem("access_token");
-        console.log(accessToken);
-        axios.get('http://localhost:8080/me',
-        {
-          headers:{
-            Authorization: `Bearer ${accessToken}`
-          },
-        })
-        .then(res => {
-          console.log(res.status);
-          if(res.status === 200){
-              console.log('access_token이 유효합니다.');
-              navigate("/BacklogCreate");
-          }
-          
-        })
-        .catch((error)=> {
-          console.log(error);
-          if(error.response.status === 401 ){
-            console.log('401 에러입니다');
-            getRefresh();
-          }
-        })
-    
-        function getRefresh(){
-            const refreshToken = localStorage.getItem("refresh_token");
-            console.log(refreshToken);
-            axios.post('http://localhost:8080/oauth/token?grant_type=refresh_token&refresh_token='+refreshToken,
-            {},
-            {
-                headers:{
-                'Authorization': 'Basic '+window.btoa(originData)}
-            }
-            ).then(res => {
-                console.log(res);
-                if(res.status === 200){
-                    localStorage.setItem("access_token", res.data.access_token);
-                    localStorage.setItem("refresh_token", res.data.refresh_token);
-                    navigate("/BacklogCreate");
-                }else{
-                console.log('logout');
-                navigate("/Login");
-                }
-                
-            })
-            .catch((error)=> {
-                console.log(error);
-            })
-            }
-         }
+  const navigate = useNavigate();
+  const [backlogs, setBacklogs] = useState([]);
+
+  // 만들기 버튼 누르면 이동 함수
+  function navigateBacklogCreate(){
+    navigate('/BacklogCreate');
+  }
+
+  // 
+  async function getBacklog() { 
+      try {
+        const response = await axios.get('http://localhost:8080/backlogs');
+        //console.log(response.data);
+        setBacklogs(response.data);
+    } catch (error) {
+        console.log(error);
+      }
+    }
+      getBacklog();
 
     return(
+        <div>
+          <div className='backlogselect-header'>
+              <div className = "backlogselect-text">
+                  백로그
+              </div>
+              <button onClick = {navigateBacklogCreate} className="backlog-btncrt"> 백로그 만들기</button>
+          </div>
 
-        <div className='backlogselect-header'>
-            <div className = "backlogselect-text">
-                백로그
-            </div>
-            <button className="backlog-btncrt" onClick ={getMe}> 백로그 만들기</button>
+          <div className="backlogselect-list">
+            <Container>
+              <Row>
+                {backlogs && backlogs.map((backlog) =>(
+                <Col className="mb-4" key = {backlog.seq}>
+                  <div className='backlogselect-card'>
+
+                    <div className='backlogselect-card-title'>
+                      {backlog.title}
+                    </div>
+
+                    <div className = 'backlogselect-card-content'>
+                      {backlog.description}
+                    </div>
+
+                    <div className = 'backlogselect-card-deadline'>
+                      deadline
+                      {backlog.deadline}
+                    </div>
+
+                    <div className ='backlogselect-card-progressbartxt'>
+                      <p>Progressbar</p>
+                    </div>
+
+                    <div className="progress mb-4">
+                      <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        25%
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                ) )}
+              </Row>
+            </Container>
+          </div>
         </div>
     );
 
