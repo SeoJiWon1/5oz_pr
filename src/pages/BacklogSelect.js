d784e85ed5ba6977389f4af26eda198b1d879195
Seo_Ji Won <120435947+SeoJiWon1@users.noreply.github.com>
2023-05-12 20:02:01 +0900

    backlog.projectTitle 추가

--- src/pages/BacklogSelect.js
+++ src/pages/BacklogSelect.js
@@ -22,16 +22,25 @@ function BacklogSelect(){
   }
 
   // 
-  async function getBacklog() { 
+  useEffect(() => {
+    async function getBacklog() {
       try {
-        const response = await axios.get('http://localhost:8080/backlogs');
-        //console.log(response.data);
+        const accessToken = localStorage.getItem("access_token");
+        const response = await axios.get('http://localhost:8080/backlogs',
+        {
+          headers:{
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+        
         setBacklogs(response.data);
-    } catch (error) {
+        console.log(response.data);
+      } catch (error) {
         console.log(error);
       }
     }
-      getBacklog();
+    getBacklog();
+  }, []);
 
     return(
         <div>
@@ -45,32 +54,37 @@ function BacklogSelect(){
           <div className="backlogselect-list">
             <Container>
               <Row>
-                {backlogs && backlogs.map((backlog) =>(
+                {backlogs && backlogs.map((backlog, index) =>(
                 <Col className="mb-4" key = {backlog.seq}>
                   <div className='backlogselect-card'>
 
                     <div className='backlogselect-card-title'>
-                      {backlog.title}
+                      <h2>{backlog.title}</h2>
                     </div>
 
                     <div className = 'backlogselect-card-content'>
-                      {backlog.description}
+                      <p>목표: {backlog.description}</p>
                     </div>
 
                     <div className = 'backlogselect-card-deadline'>
-                      deadline
+                      deadline: 
                       {backlog.deadline}
                     </div>
 
-                    <div className ='backlogselect-card-progressbartxt'>
-                      <p>Progressbar</p>
+                    <div className = 'backlogselect-card-deadline'>
+                      projetTitle: 
+                      {backlog.projectTitle}
                     </div>
 
-                    <div className="progress mb-4">
+                    {/* <div className ='backlogselect-card-progressbartxt'>
+                      <p>Progressbar</p>
+                    </div> */}
+
+                    {/* <div className="progress mb-4">
                       <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                         25%
                       </div>
-                    </div>
+                    </div> */}
                   </div>
                 </Col>
                 ) )}
