e9cfec449f53c67a1a6e93f0808b12c522ba1625
Seo_Ji Won <120435947+SeoJiWon1@users.noreply.github.com>
2023-05-14 02:50:38 +0900

    Update Slidebar.js

--- src/pages/Slidebar.js
+++ src/pages/Slidebar.js
@@ -7,41 +7,41 @@ import './Slidebar.css';
 function Slidebar() {
   return (
     <div className = "slidebar-all">
-    <Carousel variant="dark">   
-      <Carousel.Item>
-        <img
-          className="d-block w-400"
-          src={view1} width={400} height={600}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h5>우리 기능 사진으로 넣을 예정</h5>
-          <p></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-400"
-          src={view2} width={400} height={600}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h5>우리 기능 사진으로 넣을 예정 </h5>
-          <p></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-400"
-          src={view3} width={400} height={600}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h5>우리 기능 사진으로 넣을 예정</h5>
-          <p></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+      <Carousel variant="dark">   
+        <Carousel.Item>
+          <img
+            className="img-slidebar"
+            src={view1} 
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h5>우리 기능 사진으로 넣을 예정</h5>
+            <p></p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="img-slidebar"
+            src={view2}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h5>우리 기능 사진으로 넣을 예정 </h5>
+            <p></p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="img-slidebar"
+            src={view3} 
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h5>우리 기능 사진으로 넣을 예정</h5>
+            
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
