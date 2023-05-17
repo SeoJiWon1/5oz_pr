a00d90a18290979f1f3ec27a1171aae511752409
SeoJiWon1 <dhomp7730@gmail.com>
2023-05-17 14:15:31 +0900

    change

--- src/pages/Slidebar.js
+++ src/pages/Slidebar.js
@@ -7,41 +7,41 @@
 // function Slidebar() {
 //   return (
 //     <div className = "slidebar-all">
-//       <Carousel variant="dark">   
-//         <Carousel.Item>
-//           <img
-//             className="img-slidebar"
-//             src={} 
-//             alt="First slide"
-//           />
-//           <Carousel.Caption>
-//             <h5>우리 기능 사진으로 넣을 예정</h5>
-//             <p></p>
-//           </Carousel.Caption>
-//         </Carousel.Item>
-//         <Carousel.Item>
-//           <img
-//             className="img-slidebar"
-//             src={}
-//             alt="Second slide"
-//           />
-//           <Carousel.Caption>
-//             <h5>우리 기능 사진으로 넣을 예정 </h5>
-//             <p></p>
-//           </Carousel.Caption>
-//         </Carousel.Item>
-//         <Carousel.Item>
-//           <img
-//             className="img-slidebar"
-//             src={} 
-//             alt="Third slide"
-//           />
-//           <Carousel.Caption>
-//             <h5>우리 기능 사진으로 넣을 예정</h5>
-            
-//           </Carousel.Caption>
-//         </Carousel.Item>
-//       </Carousel>
+//     <Carousel variant="dark">   
+//       <Carousel.Item>
+//         <img
+//           className="d-block w-400"
+//           src={view1} width={400} height={600}
+//           alt="First slide"
+//         />
+//         <Carousel.Caption>
+//           <h5>우리 기능 사진으로 넣을 예정</h5>
+//           <p></p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <img
+//           className="d-block w-400"
+//           src={view2} width={400} height={600}
+//           alt="Second slide"
+//         />
+//         <Carousel.Caption>
+//           <h5>우리 기능 사진으로 넣을 예정 </h5>
+//           <p></p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//         <img
+//           className="d-block w-400"
+//           src={view3} width={400} height={600}
+//           alt="Third slide"
+//         />
+//         <Carousel.Caption>
+//           <h5>우리 기능 사진으로 넣을 예정</h5>
+//           <p></p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
 //     </div>
 //   );
 // }
